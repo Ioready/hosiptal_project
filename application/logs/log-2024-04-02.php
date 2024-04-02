84cefc28eb5e5b2987374556d145cb7474b40073
sunil-kumar-vishwakarma <82269536+sunil-kumar-vishwakarma@users.noreply.github.com>
2024-04-02 20:23:49 +0530

    logs updates file

--- application/logs/log-2024-04-02.php
+++ application/logs/log-2024-04-02.php
@@ -1,86 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-<<<<<<< HEAD
-ERROR - 2024-04-02 08:29:55 --> Severity: Warning --> Illegal string offset 'title' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 609
-ERROR - 2024-04-02 08:29:55 --> Severity: Warning --> Illegal string offset 'message' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 834
-ERROR - 2024-04-02 08:29:55 --> Severity: Warning --> Cannot assign an empty string to a string offset C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 834
-ERROR - 2024-04-02 08:29:55 --> Severity: Warning --> Illegal string offset 'identity' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 835
-ERROR - 2024-04-02 08:29:55 --> Severity: Warning --> Illegal string offset 'password' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 842
-ERROR - 2024-04-02 08:29:55 --> Severity: Warning --> Illegal string offset 'parent' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 848
-ERROR - 2024-04-02 08:29:55 --> Severity: Warning --> Illegal string offset 'title' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 849
-ERROR - 2024-04-02 08:29:55 --> 404 Page Not Found: ../modules/pwfpanel/controllers/Pwfpanel/img
-ERROR - 2024-04-02 08:30:06 --> Severity: Warning --> Illegal string offset 'title' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 609
-ERROR - 2024-04-02 08:30:37 --> Severity: Warning --> Illegal string offset 'title' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 966
-ERROR - 2024-04-02 08:30:37 --> Severity: Warning --> Illegal string offset 'title' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 609
-ERROR - 2024-04-02 08:30:37 --> Severity: Warning --> Illegal string offset 'message' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 834
-ERROR - 2024-04-02 08:30:37 --> Severity: Warning --> Illegal string offset 'identity' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 835
-ERROR - 2024-04-02 08:30:37 --> Severity: Warning --> Illegal string offset 'password' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 842
-ERROR - 2024-04-02 08:30:37 --> Severity: Warning --> Illegal string offset 'parent' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 848
-ERROR - 2024-04-02 08:30:37 --> Severity: Warning --> Illegal string offset 'title' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 849
-ERROR - 2024-04-02 08:30:42 --> Severity: Warning --> Illegal string offset 'title' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 609
-ERROR - 2024-04-02 08:45:44 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 08:52:39 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 154
-ERROR - 2024-04-02 08:52:39 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 08:52:42 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 154
-ERROR - 2024-04-02 08:52:42 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 08:52:52 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 154
-ERROR - 2024-04-02 08:52:52 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 08:54:26 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 154
-ERROR - 2024-04-02 08:54:26 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 08:54:31 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 154
-ERROR - 2024-04-02 08:54:31 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 08:55:18 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 154
-ERROR - 2024-04-02 08:55:18 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 156
-ERROR - 2024-04-02 08:55:22 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 154
-ERROR - 2024-04-02 08:55:22 --> Severity: Warning --> Illegal string offset 'all_plans' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 156
-ERROR - 2024-04-02 08:59:18 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 08:59:27 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:03:00 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:03:19 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:03:36 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:03:41 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:05:35 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:06:17 --> 404 Page Not Found: ../modules/pwfpanel/controllers/Pwfpanel/img
-ERROR - 2024-04-02 09:06:22 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:06:26 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:06:27 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:06:27 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:06:28 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:09:53 --> Could not find the language line "hospital_name_validation"
-ERROR - 2024-04-02 09:09:53 --> Could not find the language line "user_email_field_validation"
-ERROR - 2024-04-02 09:10:12 --> 404 Page Not Found: /index
-ERROR - 2024-04-02 09:12:43 --> Could not find the language line "hospital_name_validation"
-ERROR - 2024-04-02 09:12:43 --> Could not find the language line "user_email_field_validation"
-ERROR - 2024-04-02 09:13:04 --> 404 Page Not Found: /index
-ERROR - 2024-04-02 09:13:07 --> 404 Page Not Found: /index
-ERROR - 2024-04-02 09:13:24 --> 404 Page Not Found: /index
-ERROR - 2024-04-02 09:13:38 --> Could not find the language line "hospital_name_validation"
-ERROR - 2024-04-02 09:13:38 --> Could not find the language line "user_email_field_validation"
-ERROR - 2024-04-02 09:15:56 --> 404 Page Not Found: ../modules/pwfpanel/controllers/Pwfpanel/img
-ERROR - 2024-04-02 09:16:33 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:16:43 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:16:44 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:16:44 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:18:06 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:19:52 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 441
-ERROR - 2024-04-02 09:20:41 --> Severity: error --> Exception: syntax error, unexpected '<', expecting end of file C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 1784
-ERROR - 2024-04-02 09:20:43 --> Severity: error --> Exception: syntax error, unexpected '<', expecting end of file C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 1784
-ERROR - 2024-04-02 09:20:44 --> Severity: error --> Exception: syntax error, unexpected '<', expecting end of file C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 1784
-ERROR - 2024-04-02 09:20:44 --> Severity: error --> Exception: syntax error, unexpected '<', expecting end of file C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 1784
-ERROR - 2024-04-02 09:20:44 --> Severity: error --> Exception: syntax error, unexpected '<', expecting end of file C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 1784
-ERROR - 2024-04-02 09:23:36 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 439
-ERROR - 2024-04-02 09:23:42 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 439
-ERROR - 2024-04-02 09:25:46 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:29:57 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:30:37 --> Could not find the language line "hospital_name_validation"
-ERROR - 2024-04-02 09:30:37 --> Could not find the language line "user_email_field_validation"
-ERROR - 2024-04-02 09:30:53 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:31:02 --> 404 Page Not Found: /index
-ERROR - 2024-04-02 09:31:06 --> 404 Page Not Found: /index
-ERROR - 2024-04-02 09:31:51 --> 404 Page Not Found: /index
-ERROR - 2024-04-02 09:58:35 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 319
-ERROR - 2024-04-02 09:59:39 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\views\dashboard.php 458
-=======
 ERROR - 2024-04-02 07:13:32 --> Severity: Warning --> Illegal string offset 'title' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 609
 ERROR - 2024-04-02 07:13:32 --> Severity: Warning --> Illegal string offset 'message' C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 834
 ERROR - 2024-04-02 07:13:32 --> Severity: Warning --> Cannot assign an empty string to a string offset C:\xampp\htdocs\hosiptal_project\application\modules\pwfpanel\controllers\Pwfpanel.php 834
@@ -991,7 +910,6 @@ ERROR - 2024-04-02 09:45:19 --> Could not find the language line "user_email_fie
 ERROR - 2024-04-02 09:45:24 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\hosiptal_project\application\modules\patient\controllers\Patient.php 72
 ERROR - 2024-04-02 09:45:24 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\patient\controllers\Patient.php 75
 ERROR - 2024-04-02 09:45:24 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\hosiptal_project\application\modules\patient\controllers\Patient.php 91
->>>>>>> 70227fb0b4f348b6d424d056d5a22e7cf8f25453
 ERROR - 2024-04-02 10:38:27 --> Could not find the language line "description_validation"
 ERROR - 2024-04-02 10:38:55 --> Could not find the language line "hospital_name_validation"
 ERROR - 2024-04-02 10:38:55 --> Could not find the language line "user_email_field_validation"
