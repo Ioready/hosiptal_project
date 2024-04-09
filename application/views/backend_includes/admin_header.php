fd2e47341b11a05d9966f9a52996cd57140b750f
sunil-kumar-vishwakarma <82269536+sunil-kumar-vishwakarma@users.noreply.github.com>
2024-04-09 20:00:01 +0530

    add appointment notification in doctor

--- application/views/backend_includes/admin_header.php
+++ application/views/backend_includes/admin_header.php
@@ -502,7 +502,7 @@ hh
                                 </li>
 
                                 <li title="Aprove">
-                                    <a href="<?php echo site_url('notification'); ?>" class=" <?php echo (strtolower($this->router->fetch_class()) == "notification") ? "active" : "" ?>"><img src="<?php echo base_url(); ?>uploads/tasks.svg" style="height: 23px;width:23px;" alt="avatar"><span class="sidebar-nav-mini-hide text-dark">&nbsp;Aprove Appointment</span></a>
+                                    <a href="<?php echo site_url('notification/notification_list'); ?>" class=" <?php echo (strtolower($this->router->fetch_class()) == "notification") ? "active" : "" ?>"><img src="<?php echo base_url(); ?>uploads/tasks.svg" style="height: 23px;width:23px;" alt="avatar"><span class="sidebar-nav-mini-hide text-dark">&nbsp;Aprove Appointment</span></a>
                                 </li>
 
                                 <!-- <li title="DayTimeSlot">
